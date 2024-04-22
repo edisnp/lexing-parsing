f24f797a0d16237465fa1f2b210cf4f5cff14b08
edisnp <44681662+edisnp@users.noreply.github.com>
2024-04-22 21:06:03 +0200

    Add files via upload

---
+++ matlab_like_grammar/tokeni.h
@@ -0,0 +1,19 @@
+#ifndef TOKENI_H
+#define TOKENI_H
+
+#define eoi 0
+#define function_token 500
+#define id_token 501
+//#define special_character_token 502
+#define isequal_token 503
+#define end_token 504
+#define text_token 505
+
+#define for_token 507
+#define num_token 506
+#define if_token 508
+#define length_token 509
+#define compare_token 510
+#define error_token 520
+
+#endif
