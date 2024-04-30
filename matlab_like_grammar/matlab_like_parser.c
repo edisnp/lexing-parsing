02f952b40f53fae449331ed1c931af80e91bc049
edisnp <ujkanovicedis@gmail.com>
2024-04-30 03:24:53 +0200

    helper_function added

--- matlab_like_grammar/matlab_like_parser.c
+++ matlab_like_grammar/matlab_like_parser.c
@@ -31,26 +31,23 @@ void matlab_program_start() {
   if(lookahead == function_token) {
     printf("MatlabProgramStart -> function_token id = id(id) MatlabProgram\n");
     lookahead = yylex();
-    /*if(lookahead != id_token)
-      error("expect id_token");*/
+    
     helper_function(id_token, "expect id_token");
     lookahead = yylex();
-    /*if(lookahead != '=')
-      error("expect equality sign '='");*/
+   
     helper_function('=', "expect equality sign '='");
     lookahead = yylex();
-    if(lookahead != id_token)
-      error("expect id_token");
+   
+    helper_function(id_token, "expect id_token");
     lookahead = yylex();
-    if(lookahead != '(')
-      error("expect ( at the input stream");
+    
+    helper_function('(', "expect ( at the input stream");
     lookahead = yylex();
-    if(lookahead != id_token)
-      error("expect id_token");
+    helper_function(id_token, "expect id_token");
+    
     lookahead = yylex();
-    if(lookahead != ')')
-      error("expect ) at the input stream");
-
+    
+    helper_function(')', "expect ) at the input stream");
     lookahead = yylex();
     matlab_program();
   }else {
@@ -86,51 +83,48 @@ void if_statementP() {
   if(lookahead == length_token) {
     printf("IfStatementP => length_token (id) compare_token num_token error_statement end_token\n");
     lookahead = yylex();
-    if(lookahead != '(')
-      error("expected ( at the input stream");
+    helper_function('(', "expected ( at the input");
+    
     lookahead = yylex();
-    if(lookahead != id_token)
-      error("expected id_token at the input stream");
+    helper_function(id_token, "expect id_token");
+    
     lookahead = yylex();
-    if(lookahead != ')')
-      error("extected )");
+    helper_function(')', "Expected )");
+   
     lookahead = yylex();
-    if(lookahead != compare_token)
-      error("compare_token error");
+    helper_function(compare_token, "compare_token error");
     lookahead = yylex();
-    if(lookahead != num_token)
-      error("num_token expected!");
+    helper_function(num_token, "num_token expected!");
 
     lookahead = yylex();
     error_statement();
 
-    if(lookahead != end_token)
-      error("expected end_token at the end of if block!");
+    helper_function(end_token, "expected end_token at the end of if block!");
 
     lookahead = yylex();
   }else if(lookahead == isequal_token) {
     printf("IfStatement => ~isequal(id, id) ErrorStatement end_token\n");
     lookahead = yylex();
-    if(lookahead != '(')
-      error("expected (");
+
+    helper_function('(', "expected (");
+    
     lookahead = yylex();
-    if(lookahead != id_token)
-      error("id_token error");
+    helper_function(id_token, "id_token_error");
+    
     lookahead = yylex();
-    if(lookahead != ',')
-      error("separator error");
+    helper_function(',', "separator error");
+    
     lookahead = yylex();
-    if(lookahead != id_token)
-      error("expected id_token");
+    helper_function(id_token, "expected id_token");
+    
     lookahead = yylex();
-    if(lookahead != ')')
-      error("expected (");
+    helper_function(')', "expected");
 
     lookahead = yylex();
     error_statement();
 
-    if(lookahead != end_token)
-      error("expected end_token at the end of if block!");
+    helper_function(end_token, "expected end_token at the end of if block!");
+    
     lookahead = yylex();
   }else {
     error("expected length_token or isequal_token at the input");
@@ -141,11 +135,10 @@ void for_statement() {
   if(lookahead == for_token) {
     printf("ForStatement => for_token id = ForStatementP\n");
     lookahead = yylex();
-    if(lookahead != id_token)
-      error("id_token error");
+    helper_function(id_token, "id_token_error");
+    
     lookahead = yylex();
-    if(lookahead != '=')
-      error("=");
+    helper_function('=', " = error");
 
     lookahead = yylex();
     for_statementP();
@@ -158,57 +151,52 @@ void for_statementP() {
   if(lookahead == num_token) {
     printf("ForStatementP => num_token:num_token:num_token Assignment\n");
     lookahead = yylex();
-    if(lookahead != ':')
-      error(":");
+    helper_function(':', ": error");
+    
     lookahead = yylex();
-    if(lookahead != num_token)
-      error("num_token");
+    helper_function(num_token, "num_token error");
+    
     lookahead = yylex();
-    if(lookahead != ':')
-      error(":");
+    helper_function(':', ": error");
+    
     lookahead = yylex();
-    if(lookahead != num_token)
-      error("num_token");
+    helper_function(num_token, "num_token");
 
     lookahead = yylex();
     assignment();
-    if(lookahead != end_token)
-      error("end_token error");
+    helper_function(end_token, "end_token error");
+    
     lookahead = yylex();
   }else if(lookahead == length_token) {
     printf("ForStatementP => length_token(id):num_token:num_token assignment error_statement end_token\n");
     lookahead = yylex();
   
-    if(lookahead != '(')
-      error("(");
-    lookahead = yylex();
-    
-    if(lookahead != id_token)
-      error("id_token");
-    lookahead = yylex();
-
-    if(lookahead != ')')
-      error(")");
-    lookahead = yylex();
-    if(lookahead != ':')
-      error(":");
+    helper_function('(', "error (");
     
     lookahead = yylex();
-    if(lookahead != num_token)
-      error("num_token");
+    
+    helper_function(id_token, "id_token");
+    
     lookahead = yylex();
-    if(lookahead != ':')
-      error(":");
+    helper_function(')', "error )");
+    
     lookahead = yylex();
-    if(lookahead != num_token)
-      error("num_token");
+    helper_function(':', "error :");
+    
+    lookahead = yylex();
+    helper_function(num_token, "num_token");
+    
+    lookahead = yylex();
+    helper_function(':', ":");
+    
+    lookahead = yylex();
+    helper_function(num_token, "num_token");
 
     lookahead = yylex();
     assignment();
     error_statement();
 
-    if(lookahead != end_token)
-      error("end_token");
+    helper_function(end_token, "end_token");
     lookahead = yylex();
   }else {
     error("num_token or length_token expected");
@@ -219,25 +207,23 @@ void assignment() {
   if(lookahead == id_token) {
     printf("Assignment => id(num_token) = [Lista]\n");
     lookahead = yylex();
-    if(lookahead != '(')
-      error("(");
+    helper_function('(', "error (");
+    
     lookahead = yylex();
-    if(lookahead != id_token)
-      error("num_token EError");
+    helper_function(id_token, "id token error");
     lookahead = yylex();
-    if(lookahead != ')')
-      error(")");
+    helper_function(')', "error )");
+    
     lookahead = yylex();
-    if(lookahead != '=')
-      error("=");
+    helper_function('=', "error =");
+    
     lookahead = yylex();
-    if(lookahead != '[')
-      error("[");
+    helper_function('[', "error [");
 
     lookahead = yylex();
     lista();
-    if(lookahead != ']')
-      error("]");
+    helper_function(']', "error ]");
+    
     lookahead = yylex();
   }else {
     error("id_token error");
@@ -266,8 +252,8 @@ void listaPP() {
   if(lookahead == ',') {
     printf("listaPP => , num_token listaPP\n");
     lookahead = yylex();
-    if(lookahead != num_token)
-      error("num_token");
+    helper_function(num_token, "num_token error");
+    
     lookahead = yylex();
     listaPP();
   }else if(lookahead == ']') {
@@ -280,14 +266,14 @@ void error_statement() {
   if(lookahead == error_token){
     printf("error_statement => error_token(comment)\n");
     lookahead = yylex();
-    if(lookahead != '(')
-      error(")");
+    helper_function('(', "error (");
+    
     lookahead = yylex();
-    if(lookahead != text_token)
-      error("text token error");
+    helper_function(text_token, "text_token error");
+    
     lookahead = yylex();
-    if(lookahead != ')')
-      error("(");
+    helper_function(')', "error )");
+  
     lookahead = yylex();
   }else {
     error("error_statement error");
